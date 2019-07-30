b08ced47f5c87d63f5d16b75168782fea583255a
Shawn Berg <bergshawnm@gmail.com>
2019-07-29 22:39:47 -0400

    Complete entire git initiation process
    Script now not only initiates a repo on github, but also creates a new
    directory with a README, sets up the remote configuration, commits, and
    pushes to changes to remote.

--- repoinit.sh
+++ repoinit.sh
@@ -6,3 +6,15 @@ if [ $# -ne 1 ]; then
 else
     curl -u 'bergsm:806154e183fa1a25bf09a656888ce1d99fe2d848' https://api.github.com/user/repos -d "{\"name\":\"$1\", \"private\":true}"
 fi
+
+mkdir $1
+cd $1
+
+git init
+
+echo "README for $1" >> README
+
+git add -A
+git commit -m "Initial commit"
+git remote add origin git@github.com:bergsm/$1
+git push -u origin master
