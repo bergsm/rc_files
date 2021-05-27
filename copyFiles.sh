8b6de85fa8454de95eb6d78ca374253d833ff776
Shawn Berg <bergshawnm@gmail.com>
2021-05-27 11:09:38 -0400

    Updated copyFiles.sh

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cp ~/.bash_profile ~/Google\ Drive/Programming\ Files/rc_files/
-cp ~/.vimrc ~/Google\ Drive/Programming\ Files/rc_files/
-cp ~/.gitconfig ~/Google\ Drive/Programming\ Files/rc_files/
-#cp ~/Shawn.terminal ./
+cp .vimrc ~/.vimrc
+cp .bashrc ~/.bashrc
+cp i3/config ~/.i3/config
+cp i3/config ~/.config/i3/config
+cp i3blocks/config ~/.config/i3blocks/config
+cp .xinitrc ~/.xinitrc
+
